05f10007dc13b2202a8f3b09723d3fe11c2660d5
Jack Zheng <jackzheng10@yahoo.com>
2019-12-28 01:59:49 -0500

    Shows thumbnail and title for top five results

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,8 +1,12 @@
 import React from "react";
+import VideoItem from "./VideoItem.js";
 
-const VideoList = props => {
-  //props.videos = array of videos
-  return <div>{props.videos.length}</div>;
+const VideoList = ({ videos }) => {
+  const renderedList = videos.map(video => {
+    return <VideoItem video={video} />;
+  });
+
+  return <div>{renderedList}</div>;
 };
 
 export default VideoList;
